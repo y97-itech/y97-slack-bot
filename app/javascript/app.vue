2d56fdb700702a3bac06efb1531941ed1b8beb38
Morikiyo Inoha <morikiyo.inoha@gmail.com>
2019-08-11 17:41:55 +0900

    add AppBar component

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,16 +1,6 @@
 <template>
   <v-app id="app">
-    <v-app-bar absolute dark>
-      <v-toolbar-title>
-        <router-link to="/" class="toolbar-title">y97 Slack Bot</router-link>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn text><router-link to="/foo">Go to Foo</router-link></v-btn>
-        <v-btn text><router-link to="/baa">Go to Bar</router-link></v-btn>
-        <v-btn text>Link 3</v-btn>
-      </v-toolbar-items>
-    </v-app-bar>
+    <x-app-bar></x-app-bar>
     <v-content>
       <v-container fluid fill-height>
         <router-view></router-view>
@@ -20,18 +10,16 @@
 </template>
 
 <script>
+import XAppBar from './components/AppBar'
+
 export default {
   data: function () {
     return {
       message: 'Hello Vuetify!'
     }
-  }
+  },
+  components: {
+    'x-app-bar': XAppBar
+  },
 }
 </script>
-
-<style scoped>
-.toolbar-title {
-  color: inherit;
-  text-decoration: inherit;
-}
-</style>
---
+++ app/javascript/components/AppBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar absolute dark>
+    <v-toolbar-title>
+      <router-link to="/" class="toolbar-title">y97 Slack Bot</router-link>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn text><router-link to="/foo">Go to Foo</router-link></v-btn>
+      <v-btn text><router-link to="/baa">Go to Bar</router-link></v-btn>
+      <v-btn text>Link 3</v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
+
+<style scoped>
+.toolbar-title {
+  color: inherit;
+  text-decoration: inherit;
+}
+</style>
