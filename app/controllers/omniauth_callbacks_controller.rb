14b8b92924fc8f9a115740ae7b1fa1c85f317307
Morikiyo Inoha <morikiyo.inoha@gmail.com>
2019-08-12 19:49:42 +0900

    fix rubocop

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,11 +1,12 @@
-class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+# frozen_string_literal: true
 
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def slack
-    @user = User.authorized_by_slack(request.env["omniauth.auth"])
+    @user = User.authorized_by_slack(request.env['omniauth.auth'])
 
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Slack") if is_navigational_format?
+      sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: 'Slack') if is_navigational_format?
     else
       logger.debug('auth error')
       redirect_to 'error'
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,17 +13,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_08_12_012243) do
-
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "users", force: :cascade do |t|
-    t.string "provider", default: "", null: false
-    t.string "uid", default: "", null: false
-    t.string "access_token", null: false
-    t.string "bot_access_token", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'provider', default: '', null: false
+    t.string 'uid', default: '', null: false
+    t.string 'access_token', null: false
+    t.string 'bot_access_token', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
